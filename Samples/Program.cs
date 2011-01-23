70234bcb964dba7edf27da0f9f7e4bc014e301a5
Andreas Reiter <andreas.reiter@student.tugraz.at>
2011-01-23 21:15:33 +0100

    Initial commit

---
+++ Samples/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Deveck.Utils.StringUtils;
+
+namespace Deveck.Utils.Samples
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(@"These samples show you how to use the SimpleFormatter from deveck.net");
+            Console.WriteLine();
+
+            SimpleFormatter f = new SimpleFormatter();
+
+            Console.ReadLine();
+        }
+    }
+}
